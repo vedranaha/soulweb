e18c1c51012af8f465c1d202bc3f9d9a00050351
Vedrana <vedranaha@gmail.com>
2022-05-12 16:03:36 +0200

    header out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
+import TableStatic from "./TableStatic";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <TableStatic />
     </div>
   );
 }
